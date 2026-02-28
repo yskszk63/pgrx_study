4aa2fcaddf533870a102000e7130bb1b8ce0783a
yskszk63 <yskszk63@gmail.com>
2026-02-28 18:35:49 +0900

    0228

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,49 @@
-use pgrx::prelude::*;
+use pgrx::PgSqlErrorCode;
+use pgrx::pg_sys::panic::ErrorReport;
+use supabase_wrappers::prelude::*;
 
 ::pgrx::pg_module_magic!(name, version);
 
-#[pg_extern]
-fn hello_mytest() -> &'static str {
-    "Hello, mytest"
+#[wrappers_fdw(
+    version = "0.1.0",
+    author = "me",
+    error_type = "MyTestError",
+)]
+struct MyTest {}
+
+enum MyTestError {}
+
+impl From<MyTestError> for ErrorReport {
+    fn from(_value: MyTestError) -> Self {
+        ErrorReport::new(PgSqlErrorCode::ERRCODE_FDW_ERROR, "", "")
+    }
 }
 
-#[pg_extern]
-fn hello_mytest2() -> &'static str {
-    "Hello, mytest!!"
-}
+type MyTestResult<T> = Result<T, MyTestError>;
 
-#[cfg(any(test, feature = "pg_test"))]
-#[pg_schema]
-mod tests {
-    use pgrx::prelude::*;
-
-    #[pg_test]
-    fn test_hello_mytest() {
-        assert_eq!("Hello, mytest", crate::hello_mytest());
+impl ForeignDataWrapper<MyTestError> for MyTest {
+    fn new(_server: ForeignServer) -> Result<Self, MyTestError>
+    where
+        Self: Sized {
+        todo!()
     }
 
-}
-
-/// This module is required by `cargo pgrx test` invocations.
-/// It must be visible at the root of your extension crate.
-#[cfg(test)]
-pub mod pg_test {
-    pub fn setup(_options: Vec<&str>) {
-        // perform one-off initialization when the pg_test framework starts
+    fn begin_scan(
+        &mut self,
+        _quals: &[Qual],
+        _columns: &[Column],
+        _sorts: &[Sort],
+        _limit: &Option<Limit>,
+        _options: &std::collections::HashMap<String, String>,
+    ) -> Result<(), MyTestError> {
+        todo!()
     }
 
-    #[must_use]
-    pub fn postgresql_conf_options() -> Vec<&'static str> {
-        // return any postgresql.conf settings that are required for your tests
-        vec![]
+    fn iter_scan(&mut self, _row: &mut Row) -> Result<Option<()>, MyTestError> {
+        todo!()
+    }
+
+    fn end_scan(&mut self) -> Result<(), MyTestError> {
+        todo!()
     }
 }
