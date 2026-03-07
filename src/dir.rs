ceabcd32cfc287acb0ee772a8d479f651ed85b10
yskszk63 <yskszk63@gmail.com>
2026-03-07 18:07:35 +0900

    Today is 20260307

--- src/dir.rs
+++ src/dir.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::os::unix::fs::MetadataExt;
+use std::path::Path;
 use std::{fs, io};
 
 use pgrx::pg_sys::panic::ErrorReport;
@@ -11,6 +13,7 @@ use thiserror::Error;
 #[wrappers_fdw(version = "0.1.0", author = "me", error_type = "DirError")]
 struct Dir {
     cursor: Option<fs::ReadDir>,
+    dir: Option<String>,
 }
 
 #[derive(Error, Debug)]
@@ -20,6 +23,12 @@ enum DirError {
 
     #[error("{0}")]
     Io(#[from] io::Error),
+
+    #[error("Path required")]
+    PathRequired,
+
+    #[error("Must not contains dir")]
+    MustNotContainsDir,
 
     #[error("Not implemented.")]
     NotImplemented,
@@ -42,7 +51,10 @@ impl ForeignDataWrapper<DirError> for Dir {
     where
         Self: Sized,
     {
-        Ok(Self { cursor: None })
+        Ok(Self {
+            cursor: None,
+            dir: None,
+        })
     }
 
     fn begin_scan(
@@ -51,14 +63,17 @@ impl ForeignDataWrapper<DirError> for Dir {
         _columns: &[Column],
         _sorts: &[Sort],
         _limit: &Option<Limit>,
-        options: &std::collections::HashMap<String, String>,
+        options: &HashMap<String, String>,
     ) -> Result<()> {
+        report_notice(&format!("update {:?}", _columns));
+
         let Some(path) = options.get("dir") else {
             return Err(DirError::NoOption);
         };
 
         let entries = fs::read_dir(path)?;
         self.cursor = Some(entries);
+        self.dir = Some(path.to_owned());
         Ok(())
     }
 
@@ -92,17 +107,46 @@ impl ForeignDataWrapper<DirError> for Dir {
 
     fn end_scan(&mut self) -> Result<()> {
         self.cursor = None;
+        self.dir = None;
         Ok(())
     }
 
-    fn begin_modify(&mut self, _options: &std::collections::HashMap<String, String>) -> Result<()> {
-        report_notice("begin_modify");
-        Err(DirError::NotImplemented)
+    fn begin_modify(&mut self, options: &HashMap<String, String>) -> Result<()> {
+        let Some(path) = options.get("dir") else {
+            return Err(DirError::NoOption);
+        };
+
+        self.dir = Some(path.to_owned());
+        Ok(())
     }
 
     fn insert(&mut self, row: &Row) -> Result<()> {
-        report_notice(&format!("insert {:?}", row));
-        Err(DirError::NotImplemented)
+        let Some(dir) = &self.dir else {
+            return Err(DirError::NoOption);
+        };
+
+        let row = row
+            .cols
+            .iter()
+            .cloned()
+            .zip(&row.cells)
+            .collect::<HashMap<_, _>>();
+        let Some(&Some(Cell::String(path))) = row.get("path") else {
+            return Err(DirError::PathRequired);
+        };
+
+        let dir = Path::new(dir);
+        let path = dir.join(path);
+        if path.parent() != Some(dir) {
+            return Err(DirError::MustNotContainsDir);
+        };
+
+        fs::OpenOptions::new()
+            .create_new(true)
+            .write(true)
+            .open(path)?;
+        // TODO mode
+        Ok(())
     }
 
     fn update(&mut self, rowid: &Cell, new_row: &Row) -> Result<()> {
@@ -111,12 +155,28 @@ impl ForeignDataWrapper<DirError> for Dir {
     }
 
     fn delete(&mut self, rowid: &Cell) -> Result<()> {
-        report_notice(&format!("delete {:?}", rowid));
-        Err(DirError::NotImplemented)
+        report_notice(&format!("update {:?}", rowid));
+        let Some(dir) = &self.dir else {
+            return Err(DirError::NoOption);
+        };
+
+        let Cell::String(path) = rowid else {
+            return Err(DirError::PathRequired);
+        };
+
+        let dir = Path::new(dir);
+        let path = dir.join(path);
+        if path.parent() != Some(dir) {
+            return Err(DirError::MustNotContainsDir);
+        };
+
+        fs::remove_file(path)?;
+        Ok(())
     }
 
     fn end_modify(&mut self) -> Result<()> {
-        report_notice("end_modify");
+        self.cursor = None;
+        self.dir = None;
         Ok(())
     }
 }
